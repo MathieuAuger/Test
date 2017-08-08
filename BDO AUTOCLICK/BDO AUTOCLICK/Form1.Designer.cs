5ba678cf529e9d1d165577e3f3c35478aa024468
MaxG88 <maxime.gagnon.8898@gmail.com>
2017-08-08 14:37:18 -0400

    BDO Autoclick Program

---
+++ BDO AUTOCLICK/BDO AUTOCLICK/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace BDO_AUTOCLICK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LBL_Res = new System.Windows.Forms.Label();
+            this.TB_ResX = new System.Windows.Forms.TextBox();
+            this.LBL_Separator = new System.Windows.Forms.Label();
+            this.TB_ResY = new System.Windows.Forms.TextBox();
+            this.BTN_Go = new System.Windows.Forms.Button();
+            this.BTN_TestRefresh = new System.Windows.Forms.Button();
+            this.BTN_TestBuy = new System.Windows.Forms.Button();
+            this.BTN_StopAll = new System.Windows.Forms.Button();
+            this.TMR_Start = new System.Windows.Forms.Timer(this.components);
+            this.TMR_Refresh = new System.Windows.Forms.Timer(this.components);
+            this.TMR_Buy = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // LBL_Res
+            // 
+            this.LBL_Res.AutoSize = true;
+            this.LBL_Res.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.LBL_Res.Location = new System.Drawing.Point(12, 18);
+            this.LBL_Res.Name = "LBL_Res";
+            this.LBL_Res.Size = new System.Drawing.Size(114, 25);
+            this.LBL_Res.TabIndex = 0;
+            this.LBL_Res.Text = "Resolution: ";
+            // 
+            // TB_ResX
+            // 
+            this.TB_ResX.Location = new System.Drawing.Point(121, 24);
+            this.TB_ResX.Name = "TB_ResX";
+            this.TB_ResX.Size = new System.Drawing.Size(112, 20);
+            this.TB_ResX.TabIndex = 1;
+            this.TB_ResX.TextChanged += new System.EventHandler(this.TB_ResX_TextChanged);
+            // 
+            // LBL_Separator
+            // 
+            this.LBL_Separator.AutoSize = true;
+            this.LBL_Separator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.LBL_Separator.Location = new System.Drawing.Point(239, 23);
+            this.LBL_Separator.Name = "LBL_Separator";
+            this.LBL_Separator.Size = new System.Drawing.Size(18, 20);
+            this.LBL_Separator.TabIndex = 2;
+            this.LBL_Separator.Text = "×";
+            // 
+            // TB_ResY
+            // 
+            this.TB_ResY.Location = new System.Drawing.Point(263, 24);
+            this.TB_ResY.Name = "TB_ResY";
+            this.TB_ResY.Size = new System.Drawing.Size(112, 20);
+            this.TB_ResY.TabIndex = 1;
+            this.TB_ResY.TextChanged += new System.EventHandler(this.TB_ResY_TextChanged);
+            // 
+            // BTN_Go
+            // 
+            this.BTN_Go.Location = new System.Drawing.Point(17, 98);
+            this.BTN_Go.Name = "BTN_Go";
+            this.BTN_Go.Size = new System.Drawing.Size(115, 42);
+            this.BTN_Go.TabIndex = 3;
+            this.BTN_Go.Text = "Start";
+            this.BTN_Go.UseVisualStyleBackColor = true;
+            this.BTN_Go.Click += new System.EventHandler(this.BTN_Go_Click);
+            // 
+            // BTN_TestRefresh
+            // 
+            this.BTN_TestRefresh.Location = new System.Drawing.Point(146, 98);
+            this.BTN_TestRefresh.Name = "BTN_TestRefresh";
+            this.BTN_TestRefresh.Size = new System.Drawing.Size(115, 42);
+            this.BTN_TestRefresh.TabIndex = 3;
+            this.BTN_TestRefresh.Text = "Test Refresh";
+            this.BTN_TestRefresh.UseVisualStyleBackColor = true;
+            this.BTN_TestRefresh.Click += new System.EventHandler(this.BTN_TestRefresh_Click);
+            // 
+            // BTN_TestBuy
+            // 
+            this.BTN_TestBuy.Location = new System.Drawing.Point(275, 98);
+            this.BTN_TestBuy.Name = "BTN_TestBuy";
+            this.BTN_TestBuy.Size = new System.Drawing.Size(115, 42);
+            this.BTN_TestBuy.TabIndex = 3;
+            this.BTN_TestBuy.Text = "Test Buy/Bid";
+            this.BTN_TestBuy.UseVisualStyleBackColor = true;
+            this.BTN_TestBuy.Click += new System.EventHandler(this.BTN_TestBuy_Click);
+            // 
+            // BTN_StopAll
+            // 
+            this.BTN_StopAll.Location = new System.Drawing.Point(146, 146);
+            this.BTN_StopAll.Name = "BTN_StopAll";
+            this.BTN_StopAll.Size = new System.Drawing.Size(115, 42);
+            this.BTN_StopAll.TabIndex = 3;
+            this.BTN_StopAll.Text = "STOP ALL";
+            this.BTN_StopAll.UseVisualStyleBackColor = true;
+            this.BTN_StopAll.Click += new System.EventHandler(this.BTN_StopAll_Click);
+            // 
+            // TMR_Start
+            // 
+            this.TMR_Start.Tick += new System.EventHandler(this.TMR_Start_Tick);
+            // 
+            // TMR_Refresh
+            // 
+            this.TMR_Refresh.Tick += new System.EventHandler(this.TMR_Refresh_Tick);
+            // 
+            // TMR_Buy
+            // 
+            this.TMR_Buy.Tick += new System.EventHandler(this.TMR_Buy_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(402, 191);
+            this.Controls.Add(this.BTN_TestBuy);
+            this.Controls.Add(this.BTN_StopAll);
+            this.Controls.Add(this.BTN_TestRefresh);
+            this.Controls.Add(this.BTN_Go);
+            this.Controls.Add(this.LBL_Separator);
+            this.Controls.Add(this.TB_ResY);
+            this.Controls.Add(this.TB_ResX);
+            this.Controls.Add(this.LBL_Res);
+            this.Name = "Form1";
+            this.Text = "HENTAIHAVEN.ORG";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_START;
+        private System.Windows.Forms.Button BTN_STOP;
+        private System.Windows.Forms.Label LBL_Res;
+        private System.Windows.Forms.TextBox TB_ResX;
+        private System.Windows.Forms.Label LBL_Separator;
+        private System.Windows.Forms.TextBox TB_ResY;
+        private System.Windows.Forms.Button BTN_Go;
+        private System.Windows.Forms.Button BTN_TestRefresh;
+        private System.Windows.Forms.Button BTN_TestBuy;
+        private System.Windows.Forms.Button BTN_StopAll;
+        private System.Windows.Forms.Timer TMR_Start;
+        private System.Windows.Forms.Timer TMR_Refresh;
+        private System.Windows.Forms.Timer TMR_Buy;
+    }
+}
+
---
+++ BDO AUTOCLICK/BDO AUTOCLICK/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BDO_AUTOCLICK
+{
+    public partial class Form1 : Form
+    {
+        public int xRes { get; set; }
+        public int yRes { get; set; }
+
+        public float modHighY { get; set; }
+        public int highestY { get; set; }
+        public float modLowY { get; set; }
+        public int lowestY { get; set; }
+        public float modHighX { get; set; }
+        public int highestX { get; set; }
+        public float modLowX { get; set; }
+        public int lowestX { get; set; }
+
+        public float modHighY2 { get; set; }
+        public int highestY2 { get; set; }
+        public float modLowY2 { get; set; }
+        public int lowestY2 { get; set; }
+        public float modHighX2 { get; set; }
+        public int highestX2 { get; set; }
+        public float modLowX2 { get; set; }
+        public int lowestX2 { get; set; }
+
+        public Random rand;
+        public Point point;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            xRes = 2560;
+            yRes = 1440;
+
+            rand = new Random();
+            point = new Point();
+        }
+
+        private void BTN_START_Click(object sender, EventArgs e)
+        {
+            Bot();
+        }
+
+        private void Bot()
+        {
+            TMR_Start.Stop();
+            TMR_Start.Interval = rand.Next(50, 150);
+
+            ////////////////////////////////////////////////////////
+            //Refresh button
+
+            modHighY = (float)1440 / (float)940;
+            highestY = (int)(Math.Floor((float)yRes / modHighY + 0.5));
+            modLowY = (float)1440 / (float)965;
+            lowestY = (int)(Math.Floor((float)yRes / modLowY + 0.5));
+
+            modHighX = (float)2560 / (float)1180;
+            highestX = (int)(Math.Floor((float)xRes / modHighX + 0.5));
+            modLowX = (float)2560 / (float)1052;
+            lowestX = (int)(Math.Floor((float)xRes / modLowX + 0.5));
+
+            point.X = rand.Next(lowestX, highestX);
+            point.Y = rand.Next(highestY, lowestY);
+            Cursor.Position = point;
+
+            System.Threading.Thread.Sleep(rand.Next(25, 80));
+            ////////////////////////////////////////////////////////
+
+            ////////////////////////////////////////////////////////
+            //Buy/Bid button
+
+            modHighY2 = (float)1440 / (float)508;
+            highestY2 = (int)(Math.Floor((float)yRes / modHighY2 + 0.5));
+            modLowY2 = (float)1440 / (float)545;
+            lowestY2 = (int)(Math.Floor((float)yRes / modLowY2 + 0.5));
+
+            modHighX2 = (float)2560 / (float)1650;
+            highestX2 = (int)(Math.Floor((float)xRes / modHighX2 + 0.5));
+            modLowX2 = (float)2560 / (float)1595;
+            lowestX2 = (int)(Math.Floor((float)xRes / modLowX2 + 0.5)); ;
+
+            point.X = rand.Next(lowestX2, highestX2);
+            point.Y = rand.Next(highestY2, lowestY2);
+            Cursor.Position = point;
+
+            System.Threading.Thread.Sleep(rand.Next(25, 80));
+            ////////////////////////////////////////////////////////
+
+        }
+
+        private void TestRefresh()
+        {
+
+            TMR_Refresh.Stop();
+            TMR_Refresh.Interval = rand.Next(50, 150);
+
+            for (int i = 0; i < 20; ++i)
+            {
+                ////////////////////////////////////////////////////////
+                //Refresh button
+
+                modHighY = (float)1440 / (float)940;
+                highestY = (int)(Math.Floor((float)yRes / modHighY + 0.5));
+                modLowY = (float)1440 / (float)965;
+                lowestY = (int)(Math.Floor((float)yRes / modLowY + 0.5));
+
+                modHighX = (float)2560 / (float)1180;
+                highestX = (int)(Math.Floor((float)xRes / modHighX + 0.5));
+                modLowX = (float)2560 / (float)1052;
+                lowestX = (int)(Math.Floor((float)xRes / modLowX + 0.5));
+
+                point.X = rand.Next(lowestX, highestX);
+                point.Y = rand.Next(highestY, lowestY);
+                Cursor.Position = point;
+
+                System.Threading.Thread.Sleep(rand.Next(25, 80));
+                ////////////////////////////////////////////////////////
+            }
+        }
+
+        private void TestBuy()
+        {
+            TMR_Buy.Stop();
+            TMR_Buy.Interval = rand.Next(50, 150);
+
+
+            for (int i = 0; i < 20; ++i)
+            {
+                ////////////////////////////////////////////////////////
+                //Buy/Bid button
+
+                modHighY2 = (float)1440 / (float)508;
+                highestY2 = (int)(Math.Floor((float)yRes / modHighY2 + 0.5));
+                modLowY2 = (float)1440 / (float)545;
+                lowestY2 = (int)(Math.Floor((float)yRes / modLowY2 + 0.5));
+
+                modHighX2 = (float)2560 / (float)1650;
+                highestX2 = (int)(Math.Floor((float)xRes / modHighX2 + 0.5));
+                modLowX2 = (float)2560 / (float)1595;
+                lowestX2 = (int)(Math.Floor((float)xRes / modLowX2 + 0.5)); ;
+
+                point.X = rand.Next(lowestX2, highestX2);
+                point.Y = rand.Next(highestY2, lowestY2);
+                Cursor.Position = point;
+
+                System.Threading.Thread.Sleep(rand.Next(25, 80));
+                ////////////////////////////////////////////////////////
+            }
+        }
+
+        private void TMR_Start_Tick(object sender, EventArgs e)
+        {
+            Bot();
+
+            TMR_Start.Start();
+        }
+
+        private void TMR_Refresh_Tick(object sender, EventArgs e)
+        {
+            TestRefresh();
+        }
+
+        private void TMR_Buy_Tick(object sender, EventArgs e)
+        {
+            TestBuy();
+        }
+
+        private void BTN_Go_Click(object sender, EventArgs e)
+        {
+            TMR_Start.Start();
+        }
+
+        private void BTN_TestRefresh_Click(object sender, EventArgs e)
+        {
+            TMR_Refresh.Start();
+        }
+
+        private void BTN_TestBuy_Click(object sender, EventArgs e)
+        {
+            TMR_Buy.Start();
+        }
+
+        private void BTN_StopAll_Click(object sender, EventArgs e)
+        {
+            TMR_Buy.Stop();
+            TMR_Refresh.Stop();
+            TMR_Start.Stop();
+        }
+
+        private void TB_ResX_TextChanged(object sender, EventArgs e)
+        {
+            xRes = Convert.ToInt32(TB_ResX.Text);
+        }
+
+        private void TB_ResY_TextChanged(object sender, EventArgs e)
+        {
+            yRes = Convert.ToInt32(TB_ResY.Text);
+        }
+
+    }
+}
